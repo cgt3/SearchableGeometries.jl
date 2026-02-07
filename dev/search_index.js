96a4ddf5ea32e400a365e4dd625cb6f4242f6010
Documenter.jl <documenter@juliadocs.github.io>
2026-02-07 00:57:11 +0000

    build based on 4589823

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#SearchableGeometries","page":"Home","title":"SearchableGeometries","text":"Documentation for SearchableGeometries.\n\n","category":"section"}]
+}
