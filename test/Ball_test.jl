6b13c646d9e14df5e0420e4e8d29cd9c28e87cfe
Emmanuel Kwame Ayanful <emmanuelayanful@u.boisestate.edu>
2026-02-14 09:38:02 -0700

    Tests: isContained(BV, Ball)

--- test/Ball_test.jl
+++ test/Ball_test.jl
@@ -183,7 +183,7 @@ end
 # `isContained(BV, Ball)` --------------------------------------------------
 @testset "isContained(BV, Ball): Interior Ball" begin
     bv = BoundingVolume([0, 0], [1, 1])
-    # Ball centered at 0.5 with radius 0.25 -> bounds [0.25, 0.75]. STRICTLY INSIDE.
+    # Ball centered at [0.5, 0.5] with radius 0.25 -> bounds [0.25, 0.25], [0.75, 0.75]. STRICTLY INSIDE.
     ball_p1 = Ball([0.5, 0.5], 0.25, p=1)
     ball_p2 = Ball([0.5, 0.5], 0.25, p=2)
     ball_pInf = Ball([0.5, 0.5], 0.25, p=Inf)
@@ -199,7 +199,7 @@ end
 
 @testset "isContained(BV, Ball): Boundary Ball" begin
     bv = BoundingVolume([0, 0], [1, 1])
-    # Ball centered at 0.5 with radius 0.5 -> bounds [0.0, 1.0]. MATCHES BV BOUNDARY.
+    # Ball centered at [0.5, 0.5] with radius 0.5 -> bounds [0.0, 0.0], [1.0, 1.0]. MATCHES BV BOUNDARY.
     # This ball "meets the boundary" everywhere.
     ball_p1 = Ball([0.5, 0.5], 0.5, p=1)
     ball_p2 = Ball([0.5, 0.5], 0.5, p=2)
@@ -216,10 +216,10 @@ end
 
 @testset "isContained(BV, Ball): Partial Overlap Ball" begin
     bv = BoundingVolume([0, 0], [1, 1])
-    # Ball centered at 1.0 with radius 0.25 -> bounds [0.75, 1.25]. INTERSECTS BUT OVERFLOWS.
-    ball_p1 = Ball([1.0, 0.5], 0.5, p=1) 
-    ball_p2 = Ball([1.0, 0.5], 0.5, p=2)
-    ball_pInf = Ball([1.0, 0.5], 0.5, p=Inf)
+    # Ball centered at [1, 0.5] with radius 0.5 -> bounds [0.5, 0], [1.5, 1]. INTERSECTS BUT OVERFLOWS.
+    ball_p1 = Ball([1, 0.5], 0.5, p=1) 
+    ball_p2 = Ball([1, 0.5], 0.5, p=2)
+    ball_pInf = Ball([1, 0.5], 0.5, p=Inf)
 
     @test !isContained(bv, ball_p1, include_boundary=true)
     @test !isContained(bv, ball_p2, include_boundary=true)
@@ -232,10 +232,10 @@ end
 
 @testset "isContained(BV, Ball): Exterior Ball" begin
     bv = BoundingVolume([0, 0], [1, 1])
-    # Ball centered at 2.0 with radius 0.25 -> bounds [1.75, 2.25]. OUTSIDE.
-    ball_p1 = Ball([2.0, 0.5], 0.25, p=1)
-    ball_p2 = Ball([2.0, 0.5], 0.25, p=2)
-    ball_pInf = Ball([2.0, 0.5], 0.25, p=Inf)
+    # Ball centered at [2, 2] with radius 0.25 -> bounds [1.75, 1.75], [2.25, 2.25]. OUTSIDE.
+    ball_p1 = Ball([2, 2], 0.25, p=1)
+    ball_p2 = Ball([2, 2], 0.25, p=2)
+    ball_pInf = Ball([2, 2], 0.25, p=Inf)
 
     @test !isContained(bv, ball_p1, include_boundary=true)
     @test !isContained(bv, ball_p2, include_boundary=true)
